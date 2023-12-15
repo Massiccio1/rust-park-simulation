b5b405e30591f23591f92c1809b3cfa5c971e40b
Massiccio1 <girardellimassimo@gmail.com>
2023-12-15 11:17:04 +0100

    parse input type and fibonacci slow and fast

---
+++ src/main.rs
@@ -0,0 +1,81 @@
+use std::{io, str::FromStr};
+
+
+fn main() {
+    // Read a string from the user
+    loop{
+        println!(".--------------------------------.");
+
+        println!("enter a number: ");
+        let mut num:i32=0;
+        num=input_type(num);
+        println!("ok continue with: {}", num);
+
+        // let fib=fibonacci_slow(num);
+        
+        let fib2=fibonacci_slow(num, true);
+        println!("fibonacci di {num}= {fib2}");
+        let fib=fibonacci_fast(num, true);
+        println!("fibonacci di {num}= {fib}");
+    }
+
+}
+
+fn input_type<T:FromStr>(_type:T) -> T {
+    
+    loop {
+        let mut input = String::new();
+        io::stdin().read_line(&mut input).expect("Failed to read line");
+
+        // Trim whitespace and parse the string as an integer
+        let trimmed_input = input.trim();
+
+        // println!("parsing: {}" , trimmed_input);
+
+        match trimmed_input.parse::<T>() {
+            Ok(number) => {
+                // exit = true;
+                return number;
+                // println!("You entered a valid integer: {}", number);
+            }
+            Err(_) => {
+                println!("Error: invalid input type, try agin");
+            }
+        }
+    }
+
+}
+
+fn fibonacci_slow(depth:i32, verbose: bool) -> i128{
+    if depth == 0 {return 0;}
+    if depth == 1 {return 1;}
+
+
+    let sum = fibonacci_slow(depth-1, true) + fibonacci_slow(depth-2, true);
+    if verbose{
+        println!("{}", sum);
+    }
+    return sum;
+}
+
+fn fibonacci_fast(depth:i32, verbose: bool) -> i128{
+
+    if depth == 0 {return 0;}
+    if depth == 1 {return 1;}
+
+    
+
+    let mut v: Vec<i128> = vec![0,1];
+
+    let start:i32=2;
+    for i in start..depth+1{
+        let sum:i128 = v[(i-1) as usize] + v[(i-2) as usize];
+        v.push(sum);
+        if verbose{
+            println!("{}", sum);
+        }
+    }
+
+    
+    return *v.last().unwrap();
+}
---
+++ test.rs
@@ -0,0 +1,7 @@
+fn main() {
+
+    let num = vec![10, 20, 30, 40, 50];
+
+    println!("{}",num.get(1));
+
+}
